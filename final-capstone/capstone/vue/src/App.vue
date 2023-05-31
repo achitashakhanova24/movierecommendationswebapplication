e5780b4c0ffb15f5426eab54117ee670e6eaf301
Marco Brooks <byas.marco@yahoo.com>
2023-05-31 09:41:38 -0400

    updates to home page

--- final-capstone/capstone/vue/src/App.vue
+++ final-capstone/capstone/vue/src/App.vue
@@ -3,7 +3,43 @@
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+
     </div>
     <router-view />
+    <footer>
+        © COPYRIGHT 2023 - CinemaCrush.com
+    </footer>
   </div>
 </template>
+
+<style scoped>
+  #app{
+    background-image: url('../images/Posters.jpg');
+    background-size: cover;
+    display: grid;
+    grid-template-areas: 
+    "nav"
+    "router"
+    'footer';
+  }
+
+  #nav{
+    grid-area: 'nav';
+  }
+
+  router-view{
+    grid-area: 'router';
+  }
+
+  footer{
+    grid-area: 'footer';
+  }
+
+  footer{
+    text-align: center;
+    margin-top: 65vh;
+    
+  }
+  
+
+</style>
--- final-capstone/capstone/vue/src/views/Login.vue
+++ final-capstone/capstone/vue/src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="login">
-    <form @submit.prevent="login">
+    <form @submit.prevent="login" class="form-container">
       <h1 >Please Sign In</h1>
       <div role="alert" v-if="invalidCredentials">
         Invalid username and password!
@@ -16,10 +16,28 @@
         <label for="password">Password</label>
         <input type="password" id="password" v-model="user.password" required />
       </div>
-      <button type="submit">Sign in</button>
-      <p>
-      <router-link :to="{ name: 'register' }">Need an account? Sign up.</router-link></p>
+      <div class="button-container">
+      <button type="submit">About Us</button>
+      <router-link :to='{ name: "register" }'>
+        <button type="submit">Register</button>
+      </router-link>
+      
+      <button type="submit">Login</button>
+      </div>
+
     </form>
+    <div class='about-us'>
+      <p>
+       Picture this: You've swiped on countless potential "soulmates" on Tinder, but what 
+       if there was an app that matched you with your perfect movie instead? Are you tired of 
+       endlessly scrolling through movie recommendations that don't quite hit the mark? Look no further! 
+       CinemaCrush is here to ignite your cinematic journey with a unique twist! At CinemaCrush, we believe in 
+       the power of connection and the thrill of discovering your perfect movie match. With our innovative "yes" or 
+       "no" swiping feature, you'll embark on a thrilling adventure, exploring a vast array of films tailored to your preferences. 
+       Say goodbye to wasting time on movies that don't resonate with you and embrace the excitement of finding your cinematic soulmate.
+      </p>
+    </div>
+    
   </div>
 </template>
 
@@ -67,5 +85,38 @@ export default {
 }
 label {
   margin-right: 0.5rem;
+}
+
+.form-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+.button-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 10px;
+}
+
+.button-container button {
+  margin: 0 10px;
+}
+footer{
+  text-align: center;
+}
+.about-us{
+  text-align: center;
+  font-weight: bold;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  letter-spacing: 1px;
+  margin-left: 300px;
+  margin-right: 300px;
+  font-size: 25px;
 }
 </style>
