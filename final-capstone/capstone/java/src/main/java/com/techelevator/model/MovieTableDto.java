bff7836266ee92d4cff7ec77c5475cff3208148b
Carter Akins <caakins@gmail.com>
2023-06-06 11:28:49 -0400

    Changing how table loads data and sorting by rating

--- final-capstone/capstone/java/src/main/java/com/techelevator/model/MovieTableDto.java
+++ final-capstone/capstone/java/src/main/java/com/techelevator/model/MovieTableDto.java
@@ -2,6 +2,7 @@ package com.techelevator.model;
 
 public class MovieTableDto {
     private String title;
+    public double rating;
     private String genres;
     private String releaseDate;
     private String description;
@@ -9,12 +10,21 @@ public class MovieTableDto {
 
     public MovieTableDto(){}
 
-    public MovieTableDto(String title, String genres, String releaseDate, String description, String language){
+    public MovieTableDto(String title, String genres, String releaseDate, String description, String language, double rating){
         this.title = title;
+        this.rating = rating;
         this.genres = genres;
         this.releaseDate = releaseDate;
         this.description = description;
         this.language = language;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
     }
 
     public String getTitle() {
